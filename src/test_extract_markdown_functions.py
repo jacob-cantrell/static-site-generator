a5fb93dd4ab795ff984316a02b9e4f230009d5ab
jacob-cantrell <jcantrell218@gmail.com>
2025-01-24 16:56:25 -0500

    refactored extract_markdown_functions unit tests

--- src/test_extract_markdown_functions.py
+++ src/test_extract_markdown_functions.py
@@ -1,32 +1,32 @@
 import unittest
-from extract_markdown_functions import extract_markdown_images, extract_markdown_link
+from extract_markdown_functions import extract_markdown_image, extract_markdown_link
 
 class TestExtractMarkdownFunctions(unittest.TestCase):
     def test_no_markdown_found_image(self):
         text = "This is just text"
         self.assertEqual(
-            extract_markdown_images(text),
+            extract_markdown_image(text),
             []
         )
 
     def test_one_markdown_image(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
         self.assertEqual(
-            extract_markdown_images(text),
+            extract_markdown_image(text),
             [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
         )
 
     def test_two_markdown_images(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
         self.assertEqual(
-            extract_markdown_images(text),
+            extract_markdown_image(text),
             [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
         )
 
     def test_markdown_image_with_link_markdown(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev)"
         self.assertEqual(
-            extract_markdown_images(text),
+            extract_markdown_image(text),
             []
         )
 
