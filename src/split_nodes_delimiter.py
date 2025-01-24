8ea5396f558c2f3e83973bc185e1329b88eb5091
jacob-cantrell <jcantrell218@gmail.com>
2025-01-24 10:46:39 -0500

    finalized split_nodes_delimiter function and unit tests

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -10,6 +10,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         elif not node.text.count(delimiter) == 2:
             raise Exception("Invalid Markup syntax! No matching enclosing delimiter detected!")
         
-        
-            
+        split_by_delim = node.text.split(delimiter)
+        node_list = []
+        if not split_by_delim[0] == "":
+            node_list.append(TextNode(split_by_delim[0], node.text_type, node.url))
+        node_list.append(TextNode(split_by_delim[1], text_type, node.url))
+        if not split_by_delim[2] == "":
+            node_list.append(TextNode(split_by_delim[2], node.text_type, node.url))
+
+        text_node_list.extend(node_list)
+
     return text_node_list
--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -4,19 +4,123 @@ from textnode import TextNode, TextType
 
 class  TestSplitNodesDelimiter(unittest.TestCase):
     def test_split_node_asterik_delimiter(self):
-        pass
+        nodes = [
+            TextNode("test *test text* test 2", TextType.TEXT)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test ", TextType.TEXT),
+                TextNode("test text", TextType.BOLD),
+                TextNode(" test 2", TextType.TEXT)
+            ]
+        )
 
     def test_split_node_double_asterik_delimiter(self):
-        pass
+        nodes = [
+            TextNode("test **test text** test 2", TextType.TEXT)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test ", TextType.TEXT),
+                TextNode("test text", TextType.BOLD),
+                TextNode(" test 2", TextType.TEXT)
+            ]
+        )
 
     def test_split_node_back_quote_delimiter(self):
-        pass
+        nodes = [
+            TextNode("test `test text` test 2", TextType.TEXT)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test ", TextType.TEXT),
+                TextNode("test text", TextType.BOLD),
+                TextNode(" test 2", TextType.TEXT)
+            ]
+        )
+
+    def test_split_multiple_nodes_w_delimiter(self):
+        nodes = [
+            TextNode("test *test text* test 2", TextType.TEXT),
+            TextNode("second *test* text", TextType.TEXT)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test ", TextType.TEXT),
+                TextNode("test text", TextType.BOLD),
+                TextNode(" test 2", TextType.TEXT),
+                TextNode("second ", TextType.TEXT),
+                TextNode("test", TextType.BOLD),
+                TextNode(" text", TextType.TEXT)
+            ]
+        )
 
     def test_split_node_no_closing_delimiter(self):
-        pass
+        nodes = [
+            TextNode("test `test text test 2", TextType.TEXT)
+        ]
+
+        try:
+            split_nodes_delimiter(nodes, "`", TextType.BOLD)
+        except Exception:
+            self.assertRaises(Exception)
 
     def test_split_node_beginning_of_text(self):
-        pass
+        nodes = [
+            TextNode("`test `text", TextType.TEXT)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test ", TextType.BOLD),
+                TextNode("text", TextType.TEXT)
+            ]
+        )
 
     def test_split_node_end_of_text(self):
-        pass
+        nodes = [
+            TextNode("test `text`", TextType.TEXT)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test ", TextType.TEXT),
+                TextNode("text", TextType.BOLD)
+            ]
+        )
+
+    def test_split_nodes_not_text_type(self):
+        nodes = [
+            TextNode("test `text`", TextType.BOLD)
+        ]
+
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.BOLD)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("test `text`", TextType.BOLD)
+            ]
+        )
