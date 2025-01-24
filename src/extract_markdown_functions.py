e31a72759443b3e6522fe0ceed4d42d8365816c4
jacob-cantrell <jcantrell218@gmail.com>
2025-01-24 15:49:35 -0500

    changed returning empty list for no values to None

--- src/extract_markdown_functions.py
+++ src/extract_markdown_functions.py
@@ -1,23 +1,21 @@
 import re
 
 def extract_markdown_images(text):
-    images = []
     # Find all markdown images in text
     images_found = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
     # Check if anything was found; if not, return empty list - otherwise return found
     if images_found is None:
-        return images
+        return None
 
     return images_found
 
 def extract_markdown_link(text):
-    links = []
     # Find all markdown links in text
     links_found = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
     # Check if anything was found; if not, return empty list - otherwise return found
     if links_found is None:
-        return links
+        return None
 
     return links_found
